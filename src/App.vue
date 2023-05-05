715c63df8af7f89ae786c07365eb5fc052d55396
sroikamon <132210335+sroikamon@users.noreply.github.com>
2023-05-05 14:05:13 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <router-link to="/">Home</router-link>,
+    <router-link to="/vmodel">V-Model</router-link>,
+    <router-link to="/von">V-on</router-link>,
+    <router-link to="/vbind">v-bind</router-link>,
+    <router-link to="/prop">Prop</router-link>
+    <br /><br />
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.router-link-exact-active {
+  color: red;
+}
+</style>
